c6f2069c83249b0a2d1140dded588ac540bc4862
AHMED HASSAN <151830489+AHMED1CB@users.noreply.github.com>
2026-08-03 01:02:01 +0300

    update the database schematic

--- src/scripts/url.js
+++ src/scripts/url.js
@@ -1,16 +1,16 @@
 const urlForm = document.getElementById("url-form");
 
 const linkRule = /^https?:\/\//i;
-
+const nameRule = /^(?!.*\.\.)(?!.*\.$)(?!^\.)[a-zA-Z0-9._]{3,30}$/i;
 urlForm.onsubmit = (e) => {
   const name = urlForm.name.value;
   const link = urlForm.url.value;
 
-  if (!name || name.length < 3) {
+  if (!name || !nameRule.test(name)) {
     Swal.fire({
       title: "Invalid Data",
       icon: "error",
-      text: "Invalid Name Name Should be At Least 3 letters",
+      text: "Invalid Name Name Should be : no  Spaces | No Spicial Chars | Should be At least 3 letters and at most 30 ",
       theme: localStorage.mode ?? "light",
     });
 
@@ -31,6 +31,23 @@ urlForm.onsubmit = (e) => {
 
   formData.append("name", name);
   formData.append("link", link);
+
+  fetch("/src/api/url.php", {
+    method: "POST",
+    body: formData,
+  })
+    .then((e) => e.json())
+    .then((e) => console.log(e))
+    .catch((err) => {
+      console.log(err);
+      Swal.fire({
+        title: "Something Went Wrong ",
+        icon: "error",
+        text: "Try again Later",
+        theme: localStorage.mode ?? "light",
+      });
+      return false;
+    });
 
   e.preventDefault();
 };
